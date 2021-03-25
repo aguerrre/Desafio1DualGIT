4fadb0a0068e36a41eaf8bea68956240f199ba85
aguerrre <agustinguerrero.reyes@gmail.com>
2021-03-25 13:58:05 +0100

    Version 1

---
+++ src/com/everis/main/Main.java
@@ -0,0 +1,21 @@
+package com.everis.main;
+
+/**
+ * Desafío 1 - GIT
+ * 
+ * @author aguerrre
+ *
+ */
+public class Main {
+	/**
+	 * Método principal
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hola mundo.");
+
+	}
+
+}
